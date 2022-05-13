c9bbc06a11d44dd92d1219857007152056a06064
Tom <davies.evan.tom@gmail.com>
2022-05-13 10:23:31 +0100

    .

--- m3u8fetch.py
+++ m3u8fetch.py
@@ -4,7 +4,7 @@ import requests
 def download_m3u8(url, outfile):
     playlistroot = m3u8.load(uri=url)
     urls = [play.absolute_uri for play in playlistroot.playlists]
-    playlist = m3u8.load(urls[1])
+    playlist = m3u8.load(urls[0])
     with open(outfile,"wb") as f:
         r = requests.get(playlist.segments[0].absolute_uri)
         f.write(r.content)
