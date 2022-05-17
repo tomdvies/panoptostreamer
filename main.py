98840b45da021a2f9db59141dbcef0bb749c46aa
Tom <davies.evan.tom@gmail.com>
2022-05-17 18:53:37 +0100

    .

--- main.py
+++ main.py
@@ -73,13 +73,13 @@ def save_stream2(link, user, password,name):
     if len(videos) == 1:
         shutil.copy2('tmp/out0.mp4', out_str)
     if len(videos) == 2:
-        final_clip = clips_array([[videos[0],videos[1]]]).resize((1980,1080))
+        final_clip = clips_array([[videos[0].resize(0.6),videos[1]]]).resize((1980,1080))
         final_clip.write_videofile(out_str, threads = 8, fps=24)
     else:
         blank = ColorClip((10,10), (0,0,0), duration=videos[0].duration)
         final_clip = clips_array([[videos[0].resize(0.6),blank],
-                                  # [videos[1], videos[2].resize(videos[1].size)]]) # hack for grm
-                                  [videos[1], crop(videos[2],x1=60,x2=videos[2].size[0]-60,y1=30,y2=videos[2].size[1]-30).resize(videos[1].size)]])
+                                  [videos[1], videos[2].resize(videos[1].size)]]) # hack for grm
+                                  # [videos[1], crop(videos[2],x1=60,x2=videos[2].size[0]-60,y1=30,y2=videos[2].size[1]-30).resize(videos[1].size)]]) # hack for messed up grm camera
         final_clip.write_videofile(out_str, threads = 32, fps=24)
     print("written file,",f'output/{name}.mp4')
 
@@ -8845,6 +8845,16 @@ link_arr=[]
 for x in data["d"]["Results"]:
     link_arr.append(x["ViewerUrl"])
 # link_arr = ["https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=65b557be-f61b-40eb-8136-ae2900bb8d68"]
+link_arr = ["https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=ce31b744-a8c5-48ee-a361-ae6001010cbd",
+			"https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=40612c98-0bd2-4d1d-9b70-ae600101152d",
+			"https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=917d0114-f86c-44f0-a7c8-ae6001011ea8",
+			"https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=6cec1c27-8032-4aa1-abaa-ae6001010e85",
+			"https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=3666edf2-5dfc-40d4-a05e-ae600101170a",
+			"https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=9122b2aa-6463-4900-b0a2-ae60010120a9",
+			"https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=95f94ad8-b3fa-4b98-913b-ae97007ee102",
+			"https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=23e5fe10-3099-43b8-8b33-ae600101195a",
+			"https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=85936b1f-3180-43b0-bfa6-ae97007fb69d",
+			"https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=1a172d91-161a-4cf2-aefb-ae97008079fd"]
 for x in range(len(link_arr)):
-    save_stream2(link_arr[x],user,pwd, f"GRM_Lecture_{len(link_arr) - x}")
+    save_stream2(link_arr[x],user,pwd, f"Markov_Lecture_{x+1}")
 
