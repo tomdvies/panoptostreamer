d96b02f61a1ea99188316d9de946dc3656998a99
Tom <davies.evan.tom@gmail.com>
2022-05-20 14:40:52 +0100

    .

--- main.py
+++ main.py
@@ -90,9 +90,17 @@ with open("info.txt","r") as f:
 
 # print(data["Results"][0]["ViewerUrl"])
 # link_arr = ["https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=65b557be-f61b-40eb-8136-ae2900bb8d68"]
-link_arr = ["https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=23e5fe10-3099-43b8-8b33-ae600101195a",
-			"https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=85936b1f-3180-43b0-bfa6-ae97007fb69d",
-			"https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=1a172d91-161a-4cf2-aefb-ae97008079fd"]
+link_arr = ["https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=ce31b744-a8c5-48ee-a361-ae6001010cbd",
+            "https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=40612c98-0bd2-4d1d-9b70-ae600101152d",
+            "https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=917d0114-f86c-44f0-a7c8-ae6001011ea8",
+            "https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=6cec1c27-8032-4aa1-abaa-ae6001010e85",
+            "https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=3666edf2-5dfc-40d4-a05e-ae600101170a",
+            "https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=9122b2aa-6463-4900-b0a2-ae60010120a9",
+            "https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=95f94ad8-b3fa-4b98-913b-ae97007ee102",
+            "https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=23e5fe10-3099-43b8-8b33-ae600101195a",
+            "https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=85936b1f-3180-43b0-bfa6-ae97007fb69d",
+            "https://cambridgelectures.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=1a172d91-161a-4cf2-aefb-ae97008079fd"
+            ]
 for x in range(len(link_arr)):
-    save_stream2(link_arr[x],user,pwd, f"VP_Lecture_{x+7}")
+    save_stream2(link_arr[x],user,pwd, f"VP_Lecture_{x+1}")
 
